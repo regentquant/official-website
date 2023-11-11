64e3f05c1fc65978760a8bd0fc9cc0789f9b8c16
curryyaostudio <curryyao@yhyanbao.com>
2023-11-10 16:48:06 -0800

    Update

--- draft.py
+++ draft.py
@@ -1,8 +1,9 @@
+import pyperclip
 from PIL import Image
 import pytesseract
 
 # Load the image from file
-img_path = 'Screenshot 2023-11-09 at 12.15.16 AM.png'
+img_path = 'IMG_FFB1FE9CC995-1.jpeg'
 img = Image.open(img_path)
 
 # Use tesseract to do OCR on the image
@@ -10,7 +11,7 @@ text = pytesseract.image_to_string(img)
 text_list = text.split('\n\n')
 
 lenth = len(text_list)
-interval = 9
+interval = 11
 
 date_list = text_list[:int(interval)]
 change = text_list[int(interval): int(interval*2)]
@@ -19,6 +20,54 @@ b = text_list[int(interval*3):int(interval*4)]
 c = text_list[int(interval*4):int(interval*5)]
 d = text_list[int(interval*5):int(interval*6)]
 e = text_list[int(interval*6):int(interval*7)]
+a.reverse()
+b.reverse()
+c.reverse()
+d.reverse()
+e.reverse()
 
 for z,zz,zzz,zzzz,zzzzz in zip(a,b,c,d,e):
   print(f"{z}\t{zz}\t{zzz}\t{zzzz}\t{zzzzz}")
+
+print(f"")
+
+fs = ''
+
+for i in e:
+  if "B" in i:
+    i = i.replace("I","")
+    if "+" in i:
+      string = (int(float(i.replace("B","").replace("+",""))*1000000000))
+      fs = f"{fs}{string}\n"
+    elif "-" in i:
+      string = (int(float(i.replace("B","").replace("-",""))*-1000000000))
+      fs = f"{fs}{string}\n"
+
+  elif "M" in i:
+    i = i.replace("I","")
+    if "+" in i:
+      string = (int(float(i.replace("M", "").replace("+", "")) * 1000000))
+      fs = f"{fs}{string}\n"
+
+    elif "-" in i:
+      string = (int(float(i.replace("M", "").replace("-", "")) * -1000000))
+      fs = f"{fs}{string}\n"
+
+
+  elif "K" in i:
+    i = i.replace("I","")
+    if "+" in i:
+      string = (int(float(i.replace("K", "").replace("+", "")) * 1000))
+      fs = f"{fs}{string}\n"
+
+    elif "-" in i:
+      string = (int(float(i.replace("K", "").replace("-", "")) * -1000))
+      fs = f"{fs}{string}\n"
+
+
+  else:
+    print(i)
+
+pyperclip.copy(fs)
+
+print(fs)

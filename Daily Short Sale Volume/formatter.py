df3ce6513ce44f3ba4483cde6c4245bb2b9f5a5c
Regnet Quant <curry_yao@regentquant.com>
2023-12-18 07:38:45 +0800

    U

--- Daily Short Sale Volume/formatter.py
+++ Daily Short Sale Volume/formatter.py
@@ -8,4 +8,4 @@ for file in os.listdir("../Daily Short Sale Volume"):
 container.sort()
 
 for i in container:
-    print(f'<div class="daily-short-sale-volume-container"><a href="Daily%20Short%20Sale%20Volume/{i.replace(" ","%20")}" class="rectangle-fill"><img src="LOGO/ETFS/{i.split(" Daily")[0]}.svg" alt="{i.split(" Daily")[0]} logo">{i.split(" Daily")[0]}</a><br></div>')
+    print(f'<div class="daily-short-sale-volume-container"><a href="Daily%20Short%20Sale%20Volume/STOCK/{i.replace(" ","%20")}" class="rectangle-fill"><img src="LOGO/STOCKS/{i.split(" Daily")[0]}.svg" alt="{i.split(" Daily")[0]} logo">{i.split(" Daily")[0]}</a><br></div>')

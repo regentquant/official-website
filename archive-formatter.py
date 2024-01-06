840fa762795b9d6cb1f22c0248cf555d717f73ee
Regnet Quant <curry_yao@regentquant.com>
2024-01-06 16:59:39 +0800

    Update

--- archive-formatter.py
+++ archive-formatter.py
@@ -1,68 +1,47 @@
-string = """                                          <!-- File Start -->
-                  <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-                     <div class="card">
-                        <img src="The_Economist_Covers/The_Economist_20231125.jpeg" alt="The_Economist_20231125.jpeg" class="card-img-top">
+
+cover_name = "Barrons_20231225.jpeg"
+google_link = "https://drive.google.com/open?id=1fHUA1sgmn-o1szWgFw3ZqN_oNiLrUmyv&usp=drive_copy"
+date = f"{cover_name.split('.')[0].split('_')[-1][:4]}-{cover_name.split('.')[0].split('_')[-1][4:6]}-{cover_name.split('.')[0].split('_')[-1][6:8]}"
+print(date)
+
+if "Barrons" in cover_name:
+    publisher = "Barron's"
+
+template = f'''    <!-- File Start -->
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                    <div class="card">
+                        <img src="all-covers/{cover_name}" alt="{cover_name.replace('.jpeg','')}_Cover.jpeg"
+                             class="card-img-top">
                         <div class="card-body text-center">
-                           <h5 class="card-title">
-                              The Economist<br></br>
-                              <p>2023-11-25</p>
-                           </h5>
-                           <a href="https://drive.google.com/open?id=19SqlM8nvx1ziZBRZaHjy6Y4UAnQk8sUe&usp=drive_copy" class="btn btn-dark" style="margin-top: 10px; margin-bottom: -1px; background: black; border-width: 0px; background: white; color: #333333; border-width: 1px; border-color: #333333">
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
-  <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
-</svg>                              Preview
-                           </a>
-                           <br>
-                           <a href="https://drive.google.com/uc?export=download&id=19SqlM8nvx1ziZBRZaHjy6Y4UAnQk8sUe&usp=drive_copy" class="btn btn-dark" style="margin-top: 10px; margin-bottom: -1px; background: black; border-width: 0px">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                                 <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                                 <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                              </svg>
-                              Download
-                           </a>
+                            <h5 class="card-title">
+                                {publisher}<br></br>
+                                <p>{date}</p>
+                            </h5>
+                            <a href="{google_link}"
+                               class="btn btn-dark"
+                               style="margin-top: 10px; margin-bottom: -1px; background: black; border-width: 0px; background: white; color: #333333; border-width: 1px; border-color: #333333">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                     class="bi bi-book" viewBox="0 0 16 16">
+                                    <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                                </svg>
+                                Read Now
+                            </a>
+                            <br>
+                            <a href="{google_link.replace('open?','uc?export=download&')}"
+                               class="btn btn-dark"
+                               style="margin-top: 10px; margin-bottom: -1px; background: black; border-width: 0px">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                     class="bi bi-download" viewBox="0 0 16 16">
+                                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                                    <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                                </svg>
+                                Download
+                            </a>
                         </div>
-                     </div>
-                  </div>
-                  <!-- File End -->
-"""
-
-download_links = """https://drive.google.com/open?id=1huQrFLUCkyg_PbcjZqJhZSh-OwcmHUNK&usp=drive_copy""".split('\n')
-
-file_names = """Bloomberg_Businessweek_20231211.jpeg""".split('\n')
-
-file_names.sort()
-file_names.reverse()
-
-for file_name, download_link in zip(file_names, download_links):
-    string = """                                          <!-- File Start -->
-                  <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
-                     <div class="card">
-                        <img src="The_New_Yorker_Covers/@replace_a" alt="@replace_a" class="card-img-top">
-                        <div class="card-body text-center">
-                           <h5 class="card-title">
-                              Barron's<br></br>
-                              <p>@replace_date</p>
-                           </h5>
-                           <a href="@replace_link_a" class="btn btn-dark" style="margin-top: 10px; margin-bottom: -1px; background: black; border-width: 0px; background: white; color: #333333; border-width: 1px; border-color: #333333">
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
-  <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
-</svg>                              Preview
-                           </a>
-                           <br>
-                           <a href="@replace_link_b" class="btn btn-dark" style="margin-top: 10px; margin-bottom: -1px; background: black; border-width: 0px">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-                                 <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                                 <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                              </svg>
-                              Download
-                           </a>
-                        </div>
-                     </div>
-                  </div>
-                  <!-- File End -->
-""".replace("@replace_a", file_name.replace(".pdf", "jpeg")).replace("@replace_date",
-                                                                     f"{file_name[15:].replace('.jpeg', '')[:4]}-{file_name[15:].replace('.jpeg', '')[4:6]}-{file_name[15:].replace('.jpeg', '')[6:8]}").replace(
-        "@replace_link_a", download_link).replace("@replace_link_b",
-                                                  download_link.replace("open?", "uc?export=download&"))
-    print(string)
-    print(f"")
+                    </div>
+                </div>
+                <!-- File End -->
+                '''
+import pyperclip
+print(template)
+pyperclip.copy(template)
